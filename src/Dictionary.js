68d845068f42d36f554b38de16ddfa4853acad2c
Melina-w <meluwaigant@gmail.com>
2021-07-26 14:10:00 +1200

    create search engine

---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react"
+
+export default function Dictionary(){
+let [keyWord, setKeyWord]= useState("")
+
+function search(event){
+    event.preventDefault();
+    alert(`searching for ${keyWord} definition`);
+}
+
+function handleKeyWordChange(event){
+    setKeyWord(event.target.value);
+}
+
+    return(
+    <form onSubmit={search}>
+        <input placeholder="Type a word" type="text" className="input" autoFocus={true} onChange={handleKeyWordChange}></input>
+        <button type="search" className="btn" >Search</button>
+    </form>
+    
+    )
+}
