2ab03dcafa6db68dd955b1b702a97f240916e676
Melina-w <meluwaigant@gmail.com>
2021-07-27 22:49:50 +1200

    add call two apis

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,42 +1,51 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Dictionary(){
-let [keyWord, setKeyWord]= useState("")
+// documentation https://www.npmjs.com/package/owlbot-js
+const Owlbot = require("owlbot-js");
+const apiToken = "9b12117f96952035c3f59ffe358bac8e3665b0bd";
+const client = Owlbot(apiToken);
 
-function search(event){
+
+// documentation https://dictionaryapi.dev/
+const apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/`;
+
+export default function Dictionary() {
+  let [keyWord, setKeyWord] = useState("");
+
+  function search(event) {
     event.preventDefault();
     alert(`Searching for the definition of : ${keyWord} `);
-}
-// documentation https://dictionaryapi.dev/
-const apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
-axios.get(apiUrl).then(handleResponse)
 
-function handleKeyWordChange(event){
+
+    client.define(`${keyWord}`).then(handleResponse);
+    axios.get(apiUrl + keyWord)
+    .then((response)=> response.data)
+    .then(handleResponse)
+  }
+
+//   how to await for two promises.
+  
+  function handleKeyWordChange(event) {
     setKeyWord(event.target.value);
-}
+  }
 
-function handleResponse(response){
-console.log(response.data);
-}
+  function handleResponse(response) {
+    console.log(response);
+  }
 
-// documentation https://www.npmjs.com/package/owlbot-js
-// const Owlbot = require('owlbot-js'); 
-// const apiToken= '9b12117f96952035c3f59ffe358bac8e3665b0bd';
-// const client = Owlbot(apiToken);
- 
-// client.define(`${keyWord}`).then(function(result){
-//    console.log(result);
-// });
-
-// const apiUrl= "https://owlbot.info/api/v4/dictionary/owl -s | json_pp"
-// axios.get(apiUrl).then(handleResponse)
-
-    return(
+  return (
     <form onSubmit={search}>
-        <input placeholder="Type a word" type="text" className="input" autoFocus={true} onChange={handleKeyWordChange}></input>
-        <button type="search" className="btn" >Search</button>
+      <input
+        placeholder="Type a word"
+        type="text"
+        className="input"
+        autoFocus={true}
+        onChange={handleKeyWordChange}
+      ></input>
+      <button type="search" className="btn">
+        Search
+      </button>
     </form>
-    
-    )
+  );
 }
