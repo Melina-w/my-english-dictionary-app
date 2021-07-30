49337a2f035a44554adcc21d1a5532cb690f4b44
Melina-w <meluwaigant@gmail.com>
2021-07-30 18:42:20 +1200

    add netlify config for deployment

--- client/src/Meaning.js
+++ client/src/Meaning.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Meaning(props) {
   console.log(props.meaning.definitions[0]);
   return (
-    <div className="Meaning">
+    <div className="meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map((definitions, index) => {
         return (
