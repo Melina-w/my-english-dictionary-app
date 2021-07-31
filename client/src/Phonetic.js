43c48db044dbadb575cabc5733f3fad6aa134031
Melina-w <meluwaigant@gmail.com>
2021-07-31 21:11:39 +1200

    add target blank

--- client/src/Phonetic.js
+++ client/src/Phonetic.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Phonetic(props) {
   return (
     <div className="phonetic">
-      <a href={props.phonetic.audio}>Listen</a>
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
       {props.phonetic.text}
     </div>
   );
