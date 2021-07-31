fb16af86d975ad02a4fa31fa95fc8fcf1ce21584
Melina-w <meluwaigant@gmail.com>
2021-07-31 19:45:57 +1200

    add synonyms function to meanings

--- client/src/Meaning.js
+++ client/src/Meaning.js
@@ -1,17 +1,18 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning.definitions[0]);
   return (
     <div className="meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map((definitions, index) => {
+      {props.meaning.definitions.map((definition, index) => {
         return (
           <div key={index}>
-            <p>
-              {definitions.definition}
-              {definitions.example}
-            </p>
+            <strong>Definition: </strong>
+            {definition.definition}
+            <strong>Example: </strong>
+            {definition.example}
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
