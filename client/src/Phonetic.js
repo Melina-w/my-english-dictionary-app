662eadc2518ab73550227efc265d7b4a0cfe0194
Melina-w <meluwaigant@gmail.com>
2021-08-02 21:40:59 +1200

    add play button  to phonetic sound

--- client/src/Phonetic.js
+++ client/src/Phonetic.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function Phonetic(props) {
+  function playPronunciation(e) {
+    e.preventDefault();
+    const audio = document.getElementById("pronunciation");
+    if (!audio) return;
+    audio.play();
+  }
   return (
     <div className="phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
+      <audio src={props.phonetic.audio} id="pronunciation"></audio>
+      <FontAwesomeIcon icon={faVolumeUp} onClick={playPronunciation} />
       {props.phonetic.text}
     </div>
   );
