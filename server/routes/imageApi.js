1d3acac10bb20b854e85680a4690db3e6fcb7075
Melina-w <meluwaigant@gmail.com>
2021-08-23 18:04:06 +1200

    add image api implemented with TDD

---
+++ server/routes/imageApi.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+async function getImageOfWord(word) {
+  try {
+    const response = await axios.get(
+      `https://api.pexels.com/v1/search?query=${word}&per_page=1`,
+      {
+        headers: {
+          Authorization:
+            "563492ad6f91700001000001082a08f08aea4355b37e03e6ab66d18e",
+        },
+      }
+    );
+    return response.data;
+  } catch (err) {
+    throw new Error("Couldn't fetch image");
+  }
+}
+
+exports.getImageOfWord = getImageOfWord;
---
+++ server/routes/imageApi.test.js
@@ -0,0 +1,29 @@
+const { getImageOfWord } = require("./imageApi");
+const expectedResponse = require("./image.json");
+const axios = require("axios");
+
+jest.mock("axios");
+
+test("getImageOfWord is called by word", async () => {
+  axios.get.mockImplementation(() => {
+    return Promise.resolve({ data: expectedResponse });
+  });
+  expect(await getImageOfWord("lockdown")).toStrictEqual(expectedResponse);
+  expect(axios.get).toBeCalledWith(
+    `https://api.pexels.com/v1/search?query=lockdown&per_page=1`,
+    {
+      headers: {
+        Authorization:
+          "563492ad6f91700001000001082a08f08aea4355b37e03e6ab66d18e",
+      },
+    }
+  );
+});
+
+test("getImageOfWord return err when api call fails", async () => {
+  axios.get.mockImplementation(() => {
+    // axios has a property data, and inside it has the responseExpected
+    return Promise.reject(new Error("pexels api failure"));
+  });
+  return expect(getImageOfWord()).rejects.toThrow("Couldn't fetch image");
+});
